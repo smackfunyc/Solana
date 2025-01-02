414d232214cfdf25cd69c14efe235865a2a34103
Minh Nguyen <39523821+smackfunyc@users.noreply.github.com>
2025-01-01 23:19:42 -0500

    Add files via upload

---
+++ solf.js
@@ -0,0 +1,29 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance / LAMPORTS_PER_SOL} SOL`)
+        console.log(`To get devnet SOL, visit: https://solfaucet.com and input this address:`)
+        console.log(`${publicKey.toString()}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+main()
